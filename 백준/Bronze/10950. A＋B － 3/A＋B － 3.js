6964e8314a2159e4684d39b331135ee0034fc8a6
Seonjin <109647265+SJ-1011@users.noreply.github.com>
2025-04-03 17:06:19 +0900

    [Bronze V] Title: A+B - 3, Time: 104 ms, Memory: 9580 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const inputData = fs.readFileSync(0).toString().trim().split("\n");
+
+const t = parseInt(inputData[0]);
+
+for (let i = 1; i <= t; i++) {
+  const data = inputData[i].trim().split(" ");
+  console.log(parseInt(data[0]) + parseInt(data[1]));
+}
