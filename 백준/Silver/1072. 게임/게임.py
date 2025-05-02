7d777b9d82bd598e2706a34b5f11085d75cf3ce0
Seonjin <109647265+SJ-1011@users.noreply.github.com>
2025-05-02 18:09:31 +0900

    [Silver III] Title: 게임, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -1,21 +1,22 @@
-X, Y = map(int, input().split())
-Z = Y * 100 // X
+X, Y = map(int, input().split(" "))
+num = -1
+Z = (Y * 100) // X
+left = 0
+right = 1_000_000_000
+if Z < 99:
+  while left <= right:
+    mid = (left + right) // 2
+    
+    # 둘 값이 다르면
+    if (Y + mid) * 100 // (X + mid) > Z:
+      num = mid
+      right = mid - 1
+    
+    # 같으면
+    else:
+      left = mid + 1
+  
+  print(num)
 
-if Z >= 99:
-    print(-1)
 else:
-    left = 1
-    right = 1_000_000_000
-    answer = -1
-
-    while left <= right:
-        mid = (left + right) // 2
-        new_z = (Y + mid) * 100 // (X + mid)
-
-        if new_z > Z:
-            answer = mid
-            right = mid - 1  # 더 작은 답이 있는지 왼쪽 탐색
-        else:
-            left = mid + 1  # 승률 그대로이므로 오른쪽 탐색
-
-    print(answer)
+  print(-1)
