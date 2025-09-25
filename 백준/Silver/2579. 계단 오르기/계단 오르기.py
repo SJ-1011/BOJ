c67cb39801f0828286d96c1ace4d2a76bd64bb3c
Seonjin <109647265+SJ-1011@users.noreply.github.com>
2025-09-26 00:37:34 +0900

    [Silver III] Title: 계단 오르기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,27 +1,23 @@
 import sys
-from itertools import combinations as cb
-
 input = sys.stdin.readline
+
 stair = int(input())
-score = [0]
+points = [0]
 for _ in range(stair):
-  score.append(int(input()))
-  
-memo = [0] * (stair+1)
+    num = int(input())
+    points.append(num)
 
-if stair == 1:
-  print(score[1])
-elif stair == 2:
-  print(max(score[1] + score[2], score[2]))
-else:
-  # 첫 계단
-  memo[1] = score[1]
-  # 두 번째 계단
-  memo[2] = max(score[1] + score[2], score[2])
-  # 세 번째 계단
-  memo[3] = max(score[1] + score[3], score[2] + score[3])
+dp = [0] * (stair + 1)
 
-  for i in range(4, stair + 1):
-    memo[i] = max(memo[i - 2] + score[i], memo[i - 3] + score[i - 1] + score[i])
+dp[1] = points[1]
 
-  print(memo[stair])
+if stair > 1:
+    dp[2] = points[1] + points[2]
+
+    if stair > 2:
+        dp[3] = max(points[1] + points[3], points[2] + points[3])
+
+        for i in range(4, stair + 1):
+            dp[i] = max(dp[i-2] + points[i], dp[i-3] + points[i-1] + points[i])
+
+print(dp[stair])
