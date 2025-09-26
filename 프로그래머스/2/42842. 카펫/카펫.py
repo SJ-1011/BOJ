bb418640ae5633c29dc0743cbd68c616a875dbb1
Seonjin <109647265+SJ-1011@users.noreply.github.com>
2025-09-27 00:06:45 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,18 +1,19 @@
+import math
 def solution(brown, yellow):
-    # 8 * 2 + 6 * 2 - 4 = 24(테두리)
-    
-    size = brown + yellow
-    nums = []
     answer = []
     
-    for i in range(1, size+1):
-        if size % i == 0:
-            nums.append([i, int(size / i)])
+    full = brown + yellow
+    
+    nums = []
+    for i in range(1, int(math.sqrt(full)) + 1):
+        if (brown + yellow) % i == 0:
+            nums.append([i, full // i])
     
     for num in nums:
-        if num[0] * 2 + num[1] * 2 - 4 == brown:
-            if num[0] < num[1]:
-                num[0], num[1] = num[1], num[0]
-            answer.append(num)
-
-    return answer[0]
+        num.sort()
+        if (num[0] - 2) * (num[1] - 2) == yellow:
+            answer = [num[1], num[0]]
+            break
+    
+    
+    return answer
