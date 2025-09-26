10eaa3ac7703b80f2b5a995ba664fe9f7b9ed698
Seonjin <109647265+SJ-1011@users.noreply.github.com>
2025-09-26 17:59:58 +0900

    [level 1] Title: 모의고사, Time: 2.73 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,22 +1,29 @@
+student1 = [1, 2, 3, 4, 5]
+student2 = [2, 1, 2, 3, 2, 4, 2, 5]
+student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
 def solution(answers):
-    # 5 2 3 4 2 / 1 5 1 1
-    a = [1, 2, 3, 4, 5]
-    b = [2, 1, 2, 3, 2, 4, 2, 5]
-    c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    answer = []
     
-    a_cor = 0
-    b_cor = 0
-    c_cor = 0
+    num1 = 0
+    num2 = 0
+    num3 = 0
     
     for i in range(len(answers)):
-        if answers[i] == a[i % 5]:
-            a_cor += 1
-        if answers[i] == b[i % 8]:
-            b_cor += 1
-        if answers[i] == c[i % 10]:
-            c_cor += 1
+        if student1[i % 5] == answers[i]:
+            num1 += 1
+        if student2[i % 8] == answers[i]:
+            num2 += 1
+        if student3[i % 10] == answers[i]:
+            num3 += 1
     
-    max_cor = max(a_cor, b_cor, c_cor)
+    M = max(num1, num2, num3)
     
-    answer = [i+1 for i, x in enumerate([a_cor, b_cor, c_cor]) if x == max_cor]
+    if num1 == M:
+        answer.append(1)
+    if num2 == M:
+        answer.append(2)
+    if num3 == M:
+        answer.append(3)
+        
     return answer
