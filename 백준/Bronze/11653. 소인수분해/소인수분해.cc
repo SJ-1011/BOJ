4ab9a0005dda702f8c42abf1ca7360a07a042cd0
Seonjin <109647265+SJ-1011@users.noreply.github.com>
2025-12-13 16:40:09 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -1,46 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
+#include<iostream>
 using namespace std;
 
-bool isPrime(int);
-int main() {
+int main()
+{
     int N;
-    cin >> N;
-    vector<int> nums;
-    if (N == 1){
+
+    cin>>N;
+
+    if(N == 1)
         return 0;
-    }
 
-    int sample = N;
-    while (true){
-        for (int i = 2; i <= sqrt(sample); i++){
-            if (sample % i == 0){
-                nums.push_back(i);
-                sample = sample / i;
-                break;
-            }
-        }
-        if (isPrime(sample)){
-            nums.push_back(sample);
-            break;
+    for(int i =2;i*i<=N;i++)
+    {
+        while(N%i == 0)
+        {
+            cout<<i<<endl;
+            N /= i;
         }
     }
+    if(N>1)
+        cout<<N;
 
-    for (int x : nums) {
-        cout << x << endl;
-    }
     return 0;
-}
-
-bool isPrime(int num){
-    if (num == 2 || num == 1){
-        return true;
-    }
-    for (int i = 2; i <= sqrt(num); i++) {
-        if (num % i == 0){
-            return false;
-        }
-    }
-    return true;
 }
